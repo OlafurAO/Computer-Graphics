c16565700ac8e08a60e85954d352e713f12755c7
Ólafur <olafurao@ru.is>
2019-09-26 20:26:58 +0000

    some stuff idk

--- Programming Assignment3/main.py
+++ Programming Assignment3/main.py
@@ -7,10 +7,13 @@ from Matrix.matrix import *;
 
 screen_size = (1200, 800);
 
+# TODO: Gamepad support
+
 class Game:
     def __init__(self):
         self.init_game();
         self.init_controls();
+        self.init_level();
 
         self.shader = Shader3D();
         self.shader.render();
@@ -19,7 +22,7 @@ class Game:
         self.view_matrix = View_Matrix();
         self.projection_matrix = Projection_Matrix();
 
-        self.view_matrix.view(Point(3, 0, 3), Point(0, 0, 0), Vector(0, 1, 0));
+        self.view_matrix.view(Point(-5, 0, 3), Point(0, 0, 0), Vector(0, 1, 0));
         self.projection_matrix.set_perspective(math.pi / 2, screen_size[0] / screen_size[1], 0.5, 100);
 
         self.shader.set_view_matrix(self.view_matrix.get_matrix());
@@ -44,9 +47,10 @@ class Game:
         self.angle += math.pi * self.delta_time;
         self.mouse_pos = list(pygame.mouse.get_pos());
 
-        ###############Keyboard controls
+        ############### Keyboard controls
         if(self.w_key_pressed):
-            self.view_matrix.slide(0, 0, -self.player_speed * self.delta_time);
+            if not(self.check_collision('FORWARD')):
+                self.view_matrix.slide(0, 0, -self.player_speed * self.delta_time);
         elif(self.s_key_pressed):
             self.view_matrix.slide(0, 0, self.player_speed * self.delta_time);
 
@@ -55,12 +59,12 @@ class Game:
         elif(self.d_key_pressed):
             self.view_matrix.slide(-self.player_speed * self.delta_time, 0, 0);
 
-        ###############Reset mouse position
+        ############### Reset mouse position
         if(self.mouse_pos[0] < 100):
-            pygame.mouse.set_pos(screen_size[0] / 2, self.mouse_pos[1])
+            pygame.mouse.set_pos(screen_size[0] / 2, self.mouse_pos[1]);
             self.mouse_pos[0] = screen_size[0] / 2;
         elif(self.mouse_pos[0] > screen_size[0] - 100):
-            pygame.mouse.set_pos(screen_size[0] / 2, self.mouse_pos[1])
+            pygame.mouse.set_pos(screen_size[0] / 2, self.mouse_pos[1]);
             self.mouse_pos[0] = screen_size[0] / 2;
 
         if(self.mouse_pos[1] < 100):
@@ -70,10 +74,12 @@ class Game:
             pygame.mouse.set_pos(self.mouse_pos[0], screen_size[1] / 2);
             self.mouse_pos[1] = screen_size[1] / 2;
 
+        #print(self.view_matrix.eye.xPos, self.view_matrix.eye.yPos, self.view_matrix.eye.zPos);
+
     def display(self):
         glEnable(GL_DEPTH_TEST);
         #glDisable(GL_DEPTH_TEST);
-        glClearColor(0.0, 0.0, 1.0, 1.0);
+        glClearColor(0.0, 0.0, 0.0, 1.0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         glViewport(0, 0, screen_size[0], screen_size[1]);
 
@@ -88,32 +94,24 @@ class Game:
         pygame.display.flip();
 
     def draw_level(self):
-        ############ Wall 1
-        color = {'r': 1.0, 'g': 0.0, 'b': 1.0};
-        translation = {'x': 0.0, 'y': 0.0, 'z': -7.0}
-        scale = {'x': 15.0, 'y': 3.0, 'z': 1.0};
-        rotation_angle = {'x': 0.0, 'y': 0.0, 'z': 0.0};
+        for wall in self.level_list:
+            self.draw_cube(wall['color'], wall['translation'], wall['scale'], wall['rotation']);
 
-        self.draw_cube(color, translation, scale, rotation_angle);
-        #################################
-        ############ Wall 2
-        color = {'r': 0.0, 'g': 1.0, 'b': 1.0};
-        translation = {'x': 8.0, 'y': 0.0, 'z': 0.0}
-        scale = {'x': 15.0, 'y': 3.0, 'z': 1.0};
-        rotation_angle = {'x': 0.0, 'y': 4.75, 'z': 0.0};
+        # Rotating 3D objects
+        object_3D = {'color': {'r': 0.2, 'g': 0.2, 'b': 0.5}, 'translation': {'x': 5.0, 'y': 0.0, 'z': 5.0},
+             'scale': {'x': 1.0, 'y': 1.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 0.0, 'z': self.angle}};
 
-        self.draw_cube(color, translation, scale, rotation_angle);
-        #################################
-        '''
-        ############ Wall 3
-        color = {'r': 0.0, 'g': 1.0, 'b': 0.0};
-        translation = {'x': 0.0, 'y': 0.0, 'z': 0.0}
-        scale = {'x': 1.0, 'y': 1.0, 'z': 2.0};
-        rotation_angle = {'x': 0.0, 'y': 4.4, 'z': 0.0};
+        self.draw_cube(object_3D['color'], object_3D['translation'], object_3D['scale'], object_3D['rotation']);
 
-        self.draw_cube(color, translation, scale, rotation_angle);
-        #################################
-        '''
+        object_3D = {'color': {'r': 1.0, 'g': 0.0, 'b': 0.0}, 'translation': {'x': 5.0, 'y': 0.0, 'z': 0.0},
+                     'scale': {'x': 1.0, 'y': 1.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': self.angle, 'z': 0.0}};
+
+        self.draw_cube(object_3D['color'], object_3D['translation'], object_3D['scale'], object_3D['rotation']);
+
+        object_3D = {'color': {'r': 1.0, 'g': 0.0, 'b': 0.0}, 'translation': {'x': 5.0, 'y': 0.0, 'z': -5.0},
+                     'scale': {'x': 1.0, 'y': 1.0, 'z': 1.0}, 'rotation': {'x': self.angle, 'y': self.angle, 'z': 0.0}};
+
+        self.draw_cube(object_3D['color'], object_3D['translation'], object_3D['scale'], object_3D['rotation']);
 
     def draw_cube(self, color, trans, scale, rotation):
         self.shader.set_solid_color(color['r'], color['g'], color['b']);
@@ -154,6 +152,7 @@ class Game:
                     self.view_matrix.yaw(-self.delta_time * mouse_x_pos_movement);
                     self.mouse_pos = list(new_mouse_pos);
 
+                '''
                 if(new_mouse_pos[1] > self.mouse_pos[1]):
                     mouse_y_pos_movement = self.mouse_pos[1] - new_mouse_pos[1];
                     self.view_matrix.pitch(self.delta_time * mouse_y_pos_movement);
@@ -162,6 +161,7 @@ class Game:
                     mouse_y_pos_movement = new_mouse_pos[1] - self.mouse_pos[1];
                     self.view_matrix.pitch(-self.delta_time * mouse_y_pos_movement);
                     self.mouse_pos = list(new_mouse_pos);
+                '''
 
             if(event.type == pygame.KEYDOWN):
                 if(event.key == pygame.K_q):
@@ -194,6 +194,26 @@ class Game:
                 if (event.key == pygame.K_LSHIFT):
                     self.player_speed = 5;
 
+    def check_collision(self, direction):
+        eye = self.view_matrix.eye;
+
+        for wall in self.level_list:
+            trans = wall['translation'];
+
+            distance = (eye.xPos - trans['x'])**2 + (eye.zPos - trans['z'])**2;
+            distance = math.sqrt(distance);
+            if(distance < 2):
+                return True;
+            '''
+            if(direction == 'FORWARD'):
+                if(wall['translation']['z'] < self.view_matrix.eye.zPos):
+                    print(str(self.view_matrix.eye.zPos + self.player_speed * self.delta_time), str(wall['translation']['z']))
+                    new_zPos = self.view_matrix.eye.zPos + self.player_speed * self.delta_time;
+                    if(new_zPos <= wall['translation']['z'] + wall['scale']['z']):
+                        if(wall['translation']['x'] <= self.view_matrix.eye.xPos <= wall['translation']['x'] + wall['scale']['x']):
+                            return True;
+            '''
+
     def init_game(self):
         pygame.init();
         pygame.display.set_mode(screen_size, pygame.OPENGL | pygame.DOUBLEBUF);
@@ -215,6 +235,36 @@ class Game:
         self.a_key_pressed = False;
         self.s_key_pressed = False;
         self.d_key_pressed = False;
+
+    def init_level(self):
+        self.level_list = [
+            {'color': {'r': 1.0, 'g': 0.0, 'b': 0.0}, 'translation': {'x': 10.0, 'y': -3.0, 'z': 27.0},
+             'scale': {'x': 41.0, 'y': 3.0, 'z': 70.0}, 'rotation': {'x': 0.0, 'y': 0.0, 'z': 0.0}},
+
+            {'color': {'r': 1.0, 'g': 0.0, 'b': 1.0}, 'translation': {'x': 10.0, 'y': 0.0, 'z': -7.0},
+             'scale': {'x': 41.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 0.0, 'z': 0.0}},
+
+            {'color': {'r': 0.0, 'g': 1.0, 'b': 1.0}, 'translation': {'x': 8.0, 'y': 0.0, 'z': 0.0},
+             'scale': {'x': 15.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 4.75, 'z': 0.0}},
+
+            {'color': {'r': 0.0, 'g': 1.0, 'b': 0.0}, 'translation': {'x': 3.8, 'y': 0.0, 'z': 8.0},
+             'scale': {'x': 10.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 0.0, 'z': 0.0}},
+
+            {'color': {'r': 0.0, 'g': 1.0, 'b': 0.0}, 'translation': {'x': -10.0, 'y': 0.0, 'z': 12.0},
+             'scale': {'x': 40.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 4.75, 'z': 0.0}},
+
+            {'color': {'r': 0.0, 'g': 1.0, 'b': 0.0}, 'translation': {'x': -1.0, 'y': 0.0, 'z': 15.0},
+             'scale': {'x': 15.0, 'y': 3.0, 'z': 2.5}, 'rotation': {'x': 0.0, 'y': 4.75, 'z': 0.0}},
+
+            {'color': {'r': 0.5, 'g': 0.0, 'b': 1.0}, 'translation': {'x': 0.0, 'y': 0.0, 'z': 31.0},
+             'scale': {'x': 20.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 0.0, 'z': 0.0}},
+
+            {'color': {'r': 0.5, 'g': 0.0, 'b': 1.0}, 'translation': {'x': 10.0, 'y': 0.0, 'z': 25.0},
+             'scale': {'x': 20.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 4.75, 'z': 0.0}},
+
+            {'color': {'r': 0.5, 'g': 0.0, 'b': 1.0}, 'translation': {'x': 30.0, 'y': 0.0, 'z': 25.0},
+             'scale': {'x': 80.0, 'y': 3.0, 'z': 1.0}, 'rotation': {'x': 0.0, 'y': 4.7, 'z': 0.0}},
+        ];
 
 def main():
     game = Game();
